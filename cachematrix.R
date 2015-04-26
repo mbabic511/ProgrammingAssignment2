1275a62e027c0553bd7a544aff3f055d69794c73
mbabic511 <mbabic511@users.noreply.github.com>
2015-04-27 01:20:02 +0200

    final commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions together act as a solution for 
+## addressing potentially time-consuming computation
+## of calculating an inverse of a (large) matrix
 
-## Write a short comment describing this function
+
+## Function makeCacheMatrix initializes a list of functions. 
+## This list acts like an object containing matrix in local 
+## variable 'x' and cashed inverse of a matrix in local variable 'i'
+## Functions contained in a list are getters and setters of these
+## local variables.
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function takes a list created with a makeCacheMatrix
+## function and returns an inverse of a matrix.
+## It does so by first looking up for a cashed inverse and 
+## only calculates inverse if the cashed value could not be found.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    # returning cashed inverse
+    return(i)
+  }
+  # if cashed value not found, calculate:
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
